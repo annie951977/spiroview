942cdd87e252b0afef37f7ab50020f3467a5cd0e
Annie <liuzr.annie@gmail.com>
2023-03-21 19:03:07 -0400

    Update body of viewCategorical

--- R/viewCategorical.R
+++ R/viewCategorical.R
@@ -2,12 +2,68 @@
 #' @param
 #' @return
 #' @examples
-viewCategorical <- function() {
+viewCategorical <- function(df,
+                            demParam,
+                            spiroParam,
+                            type="box") {
 
   # takes the data frames with the categorical data and the value that you want to see
+  plotData <- df %>%
+    dpylr::select(demParam, spiroParam)
 
-  # makes ggplot graph
+  outputGraph <- ggplot(plotData, aes(x=demParam, y=spiroParam))
 
-  # export the graph
+  # makes ggplot graph based on the type of distribution graph you're looking for
+
+  if (type == "box") {
+    outputGraph <- outputGraph + geom_boxplot()
+  } else if (type == "violin") {
+    outputGraph <- outputGraph + geom_violin()
+  } else if (type == "strip") {
+    outputGraph <- outputGraph + geom_jitter()
+  }
+
+  return(outputGraph)
+}
+
+
+
+#' Produces a number of categorical plots based on the categorical demographic inputted and a certain delination
+#' @param
+#' @return
+#' @examples
+
+compareCategorical <- function(df,
+                               demParam,
+                               spiroParam,
+                               delim,
+                               type = "box") {
+
+  # takes the data frames with the categorical data and the value that you want to see
+  sel <- df %>%
+    dpylr::select(demParam, spiroParam)
+
+
+  outputGraph <- ggplot(plotData, aes(x=demParam, y=spiroParam))
+
+  # makes ggplot graph based on the type of distribution graph you're looking for
+
+  if (type == "box") {
+    outputGraph <- outputGraph + geom_boxplot()
+  } else if (type == "violin") {
+    outputGraph <- outputGraph + geom_violin()
+  } else if (type == "strip") {
+    outputGraph <- outputGraph + geom_jitter()
+  }
+
+  return(outputGraph)
 
 }
+
+viewCategoricalCounts <- function(df,
+                                  demParam,
+                                  type="pie") {
+
+}
+
+
