bae57040812fe704b552a2ada4972efe2427b172
Annie <liuzr.annie@gmail.com>
2023-03-26 13:55:14 -0400

    Added viewCategoricalCounts and added more defensive coding

--- R/viewCategorical.R
+++ R/viewCategorical.R
@@ -7,6 +7,11 @@ viewCategorical <- function(df,
                             spiroParam,
                             type="box") {
 
+  if (!is.data.frame(df) || !is.character(demParam) || !is.character(spiroParam)) {
+    stop("Please provide the proper parameters for compareNumerical")
+  }
+
+
   # takes the data frames with the categorical data and the value that you want to see
   plotData <- df %>%
     dpylr::select(demParam, spiroParam)
@@ -36,13 +41,17 @@ viewCategorical <- function(df,
 compareCategorical <- function(df,
                                demParam,
                                spiroParam,
-                               delim,
+                               delim = NULL,
                                type = "box") {
 
-  # takes the data frames with the categorical data and the value that you want to see
-  sel <- df %>%
-    dpylr::select(demParam, spiroParam)
+  if (!is.data.frame(df) || !is.character(demParam) || !is.character(spiroParam)) {
+    stop("Please provide the proper parameters for compareCategorical")
+  }
 
+
+  # takes the data frames with the categorical data and the value that you want to see
+  plotData <- df %>%
+    dpylr::select(demParam, spiroParam)
 
   outputGraph <- ggplot(plotData, aes(x=demParam, y=spiroParam))
 
@@ -56,13 +65,52 @@ compareCategorical <- function(df,
     outputGraph <- outputGraph + geom_jitter()
   }
 
+  if (delim) {
+
+  }
+
   return(outputGraph)
 
 }
 
+#' Produces graphs that show the number of samples per demographic category
+#' @param df Data that is being analyzed
+#' @param demParam A categorical demographic variable
+#' @param secondParam A second categorical demographic variable
+#' @param type The type of graph desired, options include: pie, doughnut, bar.
+#'             Default is pie
+#' @return an output graph
+#' @examples
+
 viewCategoricalCounts <- function(df,
                                   demParam,
+                                  secondParam=NULL,
                                   type="pie") {
+  if (!is.data.frame(df) || !is.character(demParam)) {
+    stop("Please provide the proper parameters for viewCaategoricalCounts")
+  }
+
+  outputGraph <- ggplot(plotData)
+
+  if(type == "bar") {
+    outputGraph <- outputGraph + geom_bar(aes(x=demParam))
+
+  } else if(type == "pie") {
+    outputGraph <- outputGraph +
+      geom_col(aes(x = 1, y = n, fill = demParam), position = "fill") +
+      coord_polar("y", start=0)
+
+  } else if(type == "doughnut") {
+    outputGraph <- outputGraph +
+      geom_bar(aes(x=demParam)) +
+      coord_polar("y", start=0) + xlim(0, 1.5)
+  }
+
+  if(!is.null(secondParam)) {
+    outGraph <- outputGraph + facet_wrap(~ secondParam)
+  }
+
+  return(outputGraph)
 
 }
 
