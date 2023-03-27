03470c903b018dd9f554fd294690a42ef6bc924f
Annie <liuzr.annie@gmail.com>
2023-03-26 23:54:12 -0400

    Basic structure of summarizeAllByCategory

--- R/summarize.R
+++ R/summarize.R
@@ -1,5 +1,175 @@
-# summarize patient
+#' Provides summary statistics of spirometry data for a specific demographic value
+#'
+#' A function that provides a table of summary statistics for a specific
+#' demographic value for the spirometry parameters inputted. This function
+#' expects a dataframe
+#'
+#' Assumption: the delimiter is a value that exists within the the domain of
+#' demParam
+#'
+#'@param df A dataframe containing each patient demographic data and at least
+#'  one mean spirometric value
+#'@param demParam Demographic category of interest
+#'@param delim
+#'@param delimIsNumeric
+#'@param spiroParam
 
-# summarize by category
 
-# summarize per sample
+summarizeAllByCategory <- function(df,
+                                   demParam,
+                                   delim,
+                                   delimIsNumeric=FALSE,
+                                   spiroParam) {
+
+  if(!is.data.frame(df) || !is.character(demParam) ||
+     !is.character(delim) || !is.vector(spiroParam)){
+    stop("Missing inputs for summarizeAllByCategory")
+  }
+
+  if(grep("\\d", delim) & !delimIsNumeric){
+    stop("delim parameter and delimIsNumeric parameter in summarizeAllByCategory does not match up")
+
+  } else if(!grep("\\d", delim) & delimIsNumeric){
+    stop("delim parameter and delimIsNumeric parameter in summarizeAllByCategory does not match up")
+  }
+
+  results <- data.frame()
+
+  if(delimIsNumeric) {
+
+    # obtain the <= value
+
+    # grep for inequality operator
+    operators <- c("<", "<=", ">", ">=", "==", "!=")
+
+    opIndices <- str_locate(delim, operators)
+
+    # get the ending index
+    opIndex <- opIndices[1,2]
+    # get the numeric value
+    op <- substring(delim, 1, opIndex)
+    numBy <- as.numeric(substring(delim, opIndex + 1, length(delim)))
+
+
+    # filter for the param
+    if (op == "<") {
+
+      # filter for the specific parameter using dplyr
+
+      # count
+
+      # calculate mean
+
+      # standard deviation
+
+      # mode
+
+      # min
+
+      # max
+
+    } else if(op == "<=") {
+
+      # filter for the specific parameter using dplyr
+
+      # count
+
+      # calculate mean
+
+      # standard deviation
+
+      # mode
+
+      # min
+
+      # max
+
+    } else if(op == ">") {
+
+      # filter for the specific parameter using dplyr
+
+      # count
+
+      # calculate mean
+
+      # standard deviation
+
+      # mode
+
+      # min
+
+      # max
+
+    } else if(op == ">=") {
+
+      # filter for the specific parameter using dplyr
+
+      # count
+
+      # calculate mean
+
+      # standard deviation
+
+      # mode
+
+      # min
+
+      # max
+
+
+    } else if(op == "==") {
+
+      # filter for the specific parameter using dplyr
+
+      # count
+
+      # calculate mean
+
+      # standard deviation
+
+      # mode
+
+      # min
+
+      # max
+
+    } else if(op == "!="){
+
+      # filter for the specific parameter using dplyr
+
+      # count
+
+      # calculate mean
+
+      # standard deviation
+
+      # mode
+
+      # min
+
+      # max
+
+    }
+
+  } else {
+
+    # filter for the specific parameter using dplyr
+
+    # count
+
+    # calculate mean
+
+    # standard deviation
+
+    # mode
+
+    # min
+
+    # max
+
+  }
+
+  return(results)
+
+}
+
