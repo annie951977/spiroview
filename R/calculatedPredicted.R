7464d7b104d100da1dce4b658a40b9a01b856f9d
Annie <liuzr.annie@gmail.com>
2023-03-29 00:38:59 -0400

    fixed calculatePctPret

--- R/calculatedPredicted.R
+++ R/calculatedPredicted.R
@@ -63,16 +63,29 @@ calculateLLNPret <- function(df,
   }
 }
 
-#' Calculates the predicted lower limit of normal based off of inputted parameters
-#' @param df A dataframe containing colnames age and height at the minimum
-#' @return
+#' Calculates the percent predicted based off of inputted parameters
+#' Assumptions:
+#' - df is a dataframe row that contains at least the age and the height
+#' of the patient
+#' - gender is denoted as 1 or 2 for male or female respectively
+#' - ethnicity is denoted as a value between 1 to 5
+#'
+#' @param df A dataframe containing colnames age and height and a spirometric column
+#' matching param at the minimum
+#' @param param The spirometric metric of interest: "FEV1", "FVC", "FEV1FVC",
+#' "PEF", "FEF2575", "FEV6", "FEV1FEV6. Default is "FEV1"
+#' @param ref The reference equation to be used
+#' @return A numeric vector or data.frame containing length(param) columns with
+#'  rows equal to the number of rows in df
 #' @examples
 #' @import rspiro
+
 calculatePctPret <- function(df, param = "FEV1", ref = "GLI") {
   # check that there's a dataframe
   if(!is.data.frame(df)) {
     stop("Please input a dataframe into calculatePctPret")
   }
+
 
   # if the df has age, height, gender
   if(is.null(df$age) || is.null(df$height)){
@@ -87,11 +100,122 @@ calculatePctPret <- function(df, param = "FEV1", ref = "GLI") {
   } else {
     gender <- 1
   }
-  if (ref == "NHANESS3") {
-    return(rspiro::pctpred_NHANES3(age, height, gender))
+
+  if(!is.null(df$ethnicity)){
+    ethnicity <- df$ethnicity
+    if (ethnicity > 5) {
+      ethnicity <- 1
+      warning("Ethnicity value not valid for spriometry equations, setting ethnicity to default of 1")
+    }
   } else {
-    return(rspiro::LLN_GLI(age, height, gender, param))
+    ethinicity <- 1
   }
+
+  if(param == "FEV1") {
+
+    if (ref == "NHANESS3") {
+      if (ethnicity > 3) {
+        ethnicity <- 1
+        warning("Ethnicity value not valid for NHANES3 equations, setting ethnicity to default of 1")
+      }
+      return(rspiro::pctpred_NHANES3(age, height, gender, ethnicity, FEV1=df$param))
+    } else if (ref == "GLI") {
+      return(rspiro::LLN_GLI(age, height, gender, ethnicity, FEV1=df$param))
+    } else {
+      stop("Please select NHANES3 or GLI")
+    }
+
+  } else if(param == "FVC") {
+
+    if (ref == "NHANESS3") {
+      if (ethnicity > 3) {
+        ethnicity <- 1
+        warning("Ethnicity value not valid for NHANES3 equations, setting ethnicity to default of 1")
+      }
+      return(rspiro::pctpred_NHANES3(age, height, gender, ethnicity, FVC=df$param))
+    } else if (ref == "GLI") {
+      return(rspiro::LLN_GLI(age, height, gender, ethnicity, FVC=df$param))
+    } else {
+      stop("Please select NHANES3 or GLI")
+    }
+
+  } else if(param == "FEV1FVC") {
+
+    if (ref == "NHANESS3") {
+      if (ethnicity > 3) {
+        ethnicity <- 1
+        warning("Ethnicity value not valid for NHANES3 equations, setting ethnicity to default of 1")
+      }
+      return(rspiro::pctpred_NHANES3(age, height, gender, ethnicity, FEV1FVC=df$param))
+    } else if (ref == "GLI") {
+      return(rspiro::LLN_GLI(age, height, gender, ethnicity, FEV1FVC=df$param))
+    } else {
+      stop("Please select NHANES3 or GLI")
+    }
+
+  } else if(param == "PEF") {
+
+    if (ref == "NHANESS3") {
+      if (ethnicity > 3) {
+        ethnicity <- 1
+        warning("Ethnicity value not valid for NHANES3 equations, setting ethnicity to default of 1")
+      }
+      return(rspiro::pctpred_NHANES3(age, height, gender, ethnicity, PEF=df$param))
+    } else if (ref == "GLI") {
+      return(rspiro::LLN_GLI(age, height, gender, ethnicity, PEF=df$param))
+    } else {
+      stop("Please select NHANES3 or GLI")
+    }
+
+  } else if(param == "FEF2575") {
+
+    if (ref == "NHANESS3") {
+      if (ethnicity > 3) {
+        ethnicity <- 1
+        warning("Ethnicity value not valid for NHANES3 equations, setting ethnicity to default of 1")
+      }
+      return(rspiro::pctpred_NHANES3(age, height, gender, ethnicity, FEF2575=df$param))
+    } else if (ref == "GLI") {
+      return(rspiro::LLN_GLI(age, height, gender, ethnicity, FEF2575=df$param))
+    } else {
+      stop("Please select NHANES3 or GLI")
+    }
+
+  } else if(param == "FEV6") {
+
+    if (ref == "NHANESS3") {
+      if (ethnicity > 3) {
+        ethnicity <- 1
+        warning("Ethnicity value not valid for NHANES3 equations, setting ethnicity to default of 1")
+      }
+      return(rspiro::pctpred_NHANES3(age, height, gender, ethnicity, FEV6=df$param))
+    } else if (ref == "GLI") {
+      return(rspiro::LLN_GLI(age, height, gender, ethnicity, FEV6=df$param))
+    } else {
+      stop("Please select NHANES3 or GLI")
+    }
+
+  } else if(param == "FEV1FEV6") {
+
+    if (ref == "NHANESS3") {
+      if (ethnicity > 3) {
+        ethnicity <- 1
+        warning("Ethnicity value not valid for NHANES3 equations, setting ethnicity to default of 1")
+      }
+      return(rspiro::pctpred_NHANES3(age, height, gender, ethnicity, FEV1FEV6=df$param))
+    } else if (ref == "GLI") {
+      return(rspiro::LLN_GLI(age, height, gender, ethnicity, FEV1FEV6=df$param))
+    } else {
+      stop("Please select NHANES3 or GLI")
+    }
+
+  } else {
+    stop("Please enter a valid spirometry metric")
+  }
+
+
+
+
 }
 
 #' Calculates the predicted mean predicted based on inputted parameters.
