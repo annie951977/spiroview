fe12024d45eaaad35e5a81726115e068f4584ac5
Annie <liuzr.annie@gmail.com>
2023-04-04 17:25:38 -0400

    fixed formatData

--- R/formatData.R
+++ R/formatData.R
@@ -27,7 +27,7 @@
 #' # Example 2: Read in a file with a different label
 #' \dontrun{
 #' examplePath <- system.file("extdata",
-#'                             "example_dataset_1.csv",
+#'                             "example_dataset_4.csv",
 #'                             package="spiroview")
 #' resultDB <- formatData(path=examplePath,
 #'                        ageLabel= "A")
@@ -55,7 +55,7 @@ formatData <- function(path,
   # check if the file is tsv or csv
 
   if(grepl("\\.csv$", path)) {
-      df <- read.csv(path, col.names = TRUE)
+      df <- read.csv(path, header= TRUE)
     } else if(grepl("\\.tsv$", path)) {
      df <- readr::read_tsv(path, col_names = TRUE)
     } else {
@@ -69,8 +69,8 @@ formatData <- function(path,
   if (!is.null(heightLabel)){
     colnames(df)[colnames(df) == heightLabel] <- "height"
 
-  } else if (grepl("HEIGHT | height | HT", colnames(df))) {
-    colnames(df)[grep("HEIGHT | height | HT", colnames(df))] <- "height"
+  } else if (any(grepl("HEIGHT|height|HT", colnames(df)))) {
+    colnames(df)[grep("HEIGHT|height|HT", colnames(df))] <- "height"
   } else {
     warning("Height data not present. Some features may not be usable")
   }
@@ -82,27 +82,35 @@ formatData <- function(path,
   if(!is.null(genderLabel)) {
     colnames(df)[colnames(df) == genderLabel] <- "gender"
 
-  } else if(grepl("m | M | male | MALE", colnames(df))) {
-    # rename column to male
-    colnames(df)[grepl("m | M | male | MALE", colnames(df))] <- "gender"
-    df[which(df$gender) == "1"] <- "1"
-    df[which(df$gender) == "0"] <- "2"
-    colnames(df)
-  } else if (grepl("f | F| female | FEMALE", colnames(df))) {
-    # rename the column to male and flip the binary
-    colnames(df)[grep("f | F| female | FEMALE", colnames(df))] <- "gender"
-    df[which(df$gender) == "1"] <- "2"
-    df[which(df$gender) == "0"] <- "1"
-    colnames(df)
-  } else if (grepl("sex | SEX | gender | GENDER", colnames(df))) {
-    sex_options <- unique(df[,grep("sex | SEX | gender | GENDER", colnames(df))])
-    if(grep("m | M | male | MALE", sex_options)){
-      sex_options[which(grep("m | M | male | MALE", sex_options))] <- "1"
-      sex_options[which(grep("f | F | female | FEMALE", sex_options))] <- "2"
-      colnames(df)[grep("sex | SEX | gender | GENDER", colnames(df))] <- "gender"
+  } else if (any(grepl("sex|SEX|gender|GENDER", colnames(df)))) {
+
+    # if all the values in gender are numeric values then we just need to rename,
+    # suppress warnings because if the values aren't numeric, then we know this
+    # check wasn't passed
+    if(suppressWarnings(any(!is.na(sapply(df[[grep("sex|SEX|gender|GENDER", colnames(df))]], as.numeric))))){
+      colnames(df)[grep("sex|SEX|gender|GENDER", colnames(df))] <- "gender"
     } else {
-      warning("Sex data not recognized")
+      sex_options <- unique(df[,grep("sex|SEX|gender|GENDER", colnames(df))])
+      colnames(df)[grep("sex|SEX|gender|GENDER", colnames(df))] <- "gender"
+      if(any(grepl("^m|^M|^male|^MALE", sex_options))){
+        maleTag <- sex_options[grep("^m|^M|^male|^MALE", sex_options)][1]
+        df$gender[which(!(df$gender == maleTag))] <- "2"
+        df$gender[which(df$gender == maleTag)] <- "1"
+      } else {
+        warning("Sex data not recognized")
+      }
     }
+
+  } else if(any(grepl("^m|^M|^male|^MALE", colnames(df)))) {
+    # rename column to male
+    colnames(df)[grep("^m|^M|^male|^MALE", colnames(df))] <- "gender"
+    df$gender[which(df$gender == "1")] <- "1"
+    df$gender[which(df$gender == "0") ] <- "2"
+  } else if (any(grepl("^f|^F|^female|^FEMALE", colnames(df)))) {
+    # rename the column to male and flip the binary
+    colnames(df)[grep("^f|^F|^female|^FEMALE", colnames(df))] <- "gender"
+    df$gender[which(df$gender == "1")] <- "2"
+    df$gender[which(df$gender == "0")] <- "1"
   } else {
     warning("Sex data not found")
   }
@@ -111,8 +119,8 @@ formatData <- function(path,
   if (!is.null(ageLabel)){
     colnames(df)[colnames(df) == ageLabel] <- "age"
 
-  } else if (grepl("age | AGE ", colnames(df))) {
-    colnames(df)[grep("age | AGE ", colnames(df))] <- "age"
+  } else if (any(grepl("age|AGE", colnames(df)))) {
+    colnames(df)[grep("age|AGE", colnames(df))] <- "age"
   } else {
     warning("Age data not found")
   }
@@ -122,8 +130,8 @@ formatData <- function(path,
   if (!is.null(ethLabel)){
     colnames(df)[colnames(df) == ethLabel] <- "ethnicity"
 
-  } else if (grepl("ethnicity | ETHNICITY ", colnames(df))) {
-    colnames(df)[grep("ethnicity | ETHNICITY", colnames(df))] <- "ethnicity"
+  } else if (any(grepl("ethnicity|ETHNICITY ", colnames(df)))) {
+    colnames(df)[grep("ethnicity|ETHNICITY", colnames(df))] <- "ethnicity"
   } else {
     warning("Ethnicity data not found")
   }
--- tests/testthat/test-formatData.R
+++ tests/testthat/test-formatData.R
@@ -11,3 +11,32 @@ test_that("tests formatData invalid inputs for formatData", {
   expect_error(formatData(txtPath))
 
 })
+
+test_that("tests formatData with different column names", {
+
+  headers <- c("id", "gender", "age", "height", "ethnicity", "smoking", "FEV1", "FVC")
+  # capitalized headers
+  testPath2 <- system.file("extdata",
+                          "example_dataset_2.csv",
+                          package="spiroview")
+  resultDB2 <- formatData(path=testPath2)
+  expect_true(setequal(headers, colnames(resultDB2)))
+
+
+  # reformat gender column
+  testPath3 <- system.file("extdata",
+                           "example_dataset_3.csv",
+                           package="spiroview")
+  resultDB3 <- formatData(path=testPath3)
+  expect_true(setequal(headers, colnames(resultDB3)))
+  expect_true(1 %in% resultDB3$gender)
+  expect_true(2 %in% resultDB3$gender)
+
+  # column rename with label input
+  testPath4 <- system.file("extdata",
+                              "example_dataset_4.csv",
+                             package="spiroview")
+  resultDB4 <- formatData(path=testPath4,
+                         ageLabel= "A")
+  expect_true("age" %in% colnames(resultDB4))
+})
